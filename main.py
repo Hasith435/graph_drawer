33b5d51147c5137520a6fd955a000a16684cb3ff
Hasith435 <66883843+Hasith435@users.noreply.github.com>
2021-10-11 22:54:49 +0400

    some asthetical changes

--- main.py
+++ main.py
@@ -20,11 +20,12 @@ root.title('Graph Drawer')
 root.configure(bg=root_bg)
 
 #essential fonts
-style1 = tkFont.Font(family= "Comic Sans MS", size=15) #general text
+style1 = tkFont.Font(family= "corbel light", size=15) #general text
 style2 = tkFont.Font(family= "corbel light", size = 15, weight= "bold")#for enter or submit buttons
 style3 = tkFont.Font(family= "corbel light", size= 13) #for entry fields
 style4 = tkFont.Font(family="corbel bold", size= 13) #bold corbel
 style5 = tkFont.Font(family="corbel bold", size=12) #button text
+style6 = tkFont.Font(family="corbel light", size=30)#title font
 
 class HoverButton(tk.Button):
     def __init__(self, master, **kw):
@@ -45,50 +46,52 @@ def create_frame(popup, colour, row=0, column=0, padx=20, pady=20, sticky=NSEW, 
     frame.grid(row=row, column=column, sticky=sticky, columnspan=columnspan, rowspan=rowspan)
     return frame
 
-#frame contaning the equation enter fields and the enter button
-eq_enter_frame = create_frame(root, light_color_frame, pady=30, padx=50)
+#Graph drawer title
+graph_drawer_lbl = Label(root, text="GRAPH DRAWER", font=style6, fg='white', bg=root_bg)
+graph_drawer_lbl.grid(row=0, column=0, sticky="NSEW")
 
-eq_lbl = Label(eq_enter_frame, text="Equation:", font=style1, bg=light_color_frame, fg='white')
-eq_input_box = Entry(eq_enter_frame,bg="#2e2e2d", foreground="#FFFFFF", borderwidth=0, width=40,font=style3)
-enter_button = HoverButton(eq_enter_frame, text="Enter",font=style5, bg=enter_button_bg, activebackground=enter_button_hover_color, borderwidth=0, width=40)
+def eq_enter_section():
+    global eq_input_box
 
-eq_lbl.grid(row=0, column=0, sticky=W, pady=10)
-eq_input_box.grid(row=1, column=0)
-enter_button.grid(row=2, column=0, pady=10)
+    #frame contaning the equation enter fields and the enter button
+    eq_enter_frame = create_frame(root, root_bg, pady=30, padx=50, row=1)
 
-#frame containing the graph
-graph_frame = create_frame(root, root_bg, column=1, rowspan=2)
+    eq_lbl = Label(eq_enter_frame, text="EQUATION:", font=style1, bg=root_bg, fg='white')
+    eq_input_box = Entry(eq_enter_frame,bg="#2e2e2d", foreground="#FFFFFF", borderwidth=0, width=40,font=style3)
+    enter_button = HoverButton(eq_enter_frame, text="ENTER",font=style5, bg=enter_button_bg, activebackground=enter_button_hover_color, borderwidth=0, width=40, command=graph)
 
-# fig = Figure(figsize=(5,5), dpi=100)
-#
-# x = np.linspace(-5,5,100)
-#
-# plot = fig.add_subplot(111)
-# plot.plot(x, 5*x+3, '-r', label="graph")
-#
-# canvas = FigureCanvasTkAgg(fig, graph_frame)
-# canvas.draw()
-# canvas.get_tk_widget().pack()
-
-fig = Figure(figsize=(5,5), dpi=100)
-plot = fig.add_subplot(111)
-x = np.linspace(-5,5, 100)
-
-plot.plot(x, x*x, '-r',label="graph")
-plot.grid()
-plot.axhline()
-plot.axvline()
-plot.set_xlim([-6,6])
-plot.set_ylim([-6,6])
-
-canvas = FigureCanvasTkAgg(fig, graph_frame)
-canvas.draw()
-canvas.get_tk_widget().pack()
+    eq_lbl.grid(row=0, column=0, sticky=W, pady=10)
+    eq_input_box.grid(row=1, column=0)
+    enter_button.grid(row=2, column=0, pady=10)
 
 
 
 
 
+def graph():
+
+    equation = eq_input_box.get()
+    print(type(equation))
+
+    #frame containing the graph
+    graph_frame = create_frame(root, root_bg, column=1, rowspan=2)
+
+    fig = Figure(figsize=(5,5), dpi=100)
+    plot = fig.add_subplot(111)
+    x = np.linspace(-5,5, 100)
+
+    plot.plot(x, 5*x+3, '-r',label="graph")
+    plot.grid()
+    plot.axhline()
+    plot.axvline()
+    plot.set_xlim([-6,6])
+    plot.set_ylim([-6,6])
+
+    canvas = FigureCanvasTkAgg(fig, graph_frame)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+
+eq_enter_section()
 
 root.mainloop()
 
